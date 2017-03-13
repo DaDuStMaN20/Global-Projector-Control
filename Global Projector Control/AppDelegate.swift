a8bd3702e7046471b6cbce5e0a95503ec46b3f67
Dustin Wasserman <dustin2094@gmail.com>
2017-03-13 17:57:18 -0400

    Initial Commit

---
+++ Global Projector Control/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Global Projector Control
+//
+//  Created by Dustin Wasserman on 3/13/17.
+//  Copyright © 2017 Dustin Wasserman. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ Global Projector Control/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Global Projector Control
+//
+//  Created by Dustin Wasserman on 3/13/17.
+//  Copyright © 2017 Dustin Wasserman. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
